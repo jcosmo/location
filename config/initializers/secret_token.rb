4e59d3da2bb76309b64747c7a251196a82cdb26d
James Walker <github@jzwalker.id.au>
2011-02-15 22:49:12 +1100

    Rails 3 app on postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Location::Application.config.secret_token = 'ee4cca86b0d14fac162a65b8f3d6a0de2afb31ffc96051bb2d77136ddee3beda564cb7b07900e7488b5898dc56d52532f791b0d6a5dd8d6c350fbbe107eaec5f'
